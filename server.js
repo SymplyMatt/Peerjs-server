56d31ed4c58732c55aa38e199044d8360fa32192
Matt <symplymatt@gmail.com>
2023-11-02 10:11:32 +0100

    cors

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const peerServer = require('http').createServer(peerApp);
 const options = { debug: true }
 const peerPort = 8080;
 
-// Import the 'cors' library
 const cors = require('cors');
 
-// Set up CORS middleware to allow requests from your React app's origin
-peerApp.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000', 
+};
 
-peerApp.use('/peerjs', ExpressPeerServer(peerServer, options));
+peerApp.use('/peerjs', cors(corsOptions), ExpressPeerServer(peerServer, options));
 
 peerServer.listen(peerPort, () => console.log(`Peer server now working on port ${peerPort}`));
